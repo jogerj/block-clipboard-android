f806c90be48e7d96b318c426c77fe315f86f38e8
jogerj <30559735+jogerj@users.noreply.github.com>
2022-02-16 08:10:22 +0100

    check for unauthorized

--- disable_clipboard.sh
+++ disable_clipboard.sh
@@ -16,15 +16,24 @@ if [[ ! $(adb devices | grep $address | grep device) ]]; then
   adb tcpip 5555;
   conn=$(adb connect $address:5555 | grep connected);
   if [[ ! $conn ]]; then
-    echo "Click on Allow when prompted. If no prompt pops up, disable and re-enable USB debugging"
+    echo "Click on Allow when prompted. If no prompt pops up, disable and re-enable USB debugging."
     read -p "Press ENTER to continue..."
     conn=$(adb connect $address:5555);
-    echo $conn;
-    if [[ ! $(echo $conn | grep connected) ]]; then exit; fi;
+    echo "$conn";
+    if [[ ! $(echo "$conn" | grep connected) ]]; then 
+	  echo "Failed to connect to adb server.";
+	  exit;
+	fi;
+	conn="$(adb devices)"
+	if [[ $(echo "$conn" | grep $address | grep "unauthorized") ]]; then 
+	  echo "$conn";
+	  echo "Could not authorize USB debugging. Please click on allow when prompted. If problem persists, check \"Always allow from this computer\" (you can revoke this in settings later)"
+	  exit; 
+	fi;
   fi;
 fi;
 
 adb -s $address:5555 shell cmd appops query-op --user 0 READ_CLIPBOARD allow;
-echo "Select a package to disable READ_CLIPBOARD permission"
+echo "Select a package to disable READ_CLIPBOARD permission";
 read -p "Enter package name: " pkgname;
 if [[ $pkgname ]]; then adb -s $address:5555 shell cmd appops set $pkgname READ_CLIPBOARD ignore; fi;
