58cc42857c78a962219ff2db3cf0dc662f19b926
jogerj <30559735+jogerj@users.noreply.github.com>
2022-02-16 07:03:09 +0100

    use adb tcpip for android 10 support

--- disable_clipboard.sh
+++ disable_clipboard.sh
@@ -1,22 +1,23 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 
-installed=$(adb --version | grep "Android Debug Bridge");
-if [[ ! $installed ]]; then pkg install android-tools; fi;
+if [[ ! $(adb --version | grep "Android Debug Bridge") ]]; then pkg install android-tools; fi;
 
-if [[ ! $(adb devices | grep localhost | grep device) ]]; then
-  conn=$(adb connect localhost | grep connected);
+read -p "Enter hostname or IP address: [localhost] " address
+if [[ ! $address ]]; then address="localhost"; fi;
+
+if [[ ! $(adb devices | grep $address | grep device) ]]; then
+  adb tcpip 5555;
+  conn=$(adb connect $address:5555 | grep connected);
   if [[ ! $conn ]]; then
-    read -p "Enter ADB Pairing Port: [skip pairing] " pairport;
-    if [[ $pairport ]]; then adb pair localhost:$pairport; fi;
-    read -p "Enter ADB Connect Port: [5555] " connport;
-    if [[ ! $connport ]]; then connport=5555; fi;
-    conn=$(adb connect localhost:$connport);
+    echo "Click on Allow when prompted. If no prompt pops up, disable and re-enable USB debugging"
+    read -p "Press ENTER to continue..."
+    conn=$(adb connect $address);
     echo $conn;
     if [[ ! $(echo $conn | grep connected) ]]; then exit; fi;
   fi;
 fi;
 
-adb shell cmd appops query-op --user 0 READ_CLIPBOARD allow;
+adb -s $address:5555 shell cmd appops query-op --user 0 READ_CLIPBOARD allow;
 echo "Select a package to disable READ_CLIPBOARD permission"
 read -p "Enter package name: " pkgname;
-if [[ $pkgname ]]; then adb shell cmd appops set $pkgname READ_CLIPBOARD ignore; fi;
+if [[ $pkgname ]]; then adb -s $address:5555 shell cmd appops set $pkgname READ_CLIPBOARD ignore; fi;
